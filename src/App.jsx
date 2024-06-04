d6677a1b4cdb25dd89d927b149382d13f6f216e5
Ja <ja.techup@gmail.com>
2024-06-04 16:12:54 +0700

    feat: movie list

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,45 @@
 import "./App.css";
+import movies from "./data/movies";
 
 function App() {
   return (
     <div className="App">
+      <style>
+        @import
+        url('https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+      </style>
       <section className="movie-list-section">
+        <h1>Movie List Section</h1>
         {/* Render Movie Lists Here */}
+        <MovieList />
       </section>
     </div>
   );
+}
+
+function MovieList() {
+  return movies.map((movie) => {
+    return (
+      <div className="movie-card">
+        <div>
+          <img src={movie.image} width={100} height={102} />
+        </div>
+        <div className="movie-info">
+          <p>Title: {movie.title}</p>
+          <p>Year: {movie.year}</p>
+          <p>Runtime: {movie.runtime}</p>
+          <p class="genres">
+            Genres:{" "}
+            {movie.genres.map((genres) => {
+              return <div>{genres}</div>;
+            })}
+          </p>
+          <p>IMDB Ratings: {movie.imdbRating}</p>
+          <p>IMDB Votes: {movie.imdbVotes}</p>
+        </div>
+      </div>
+    );
+  });
 }
 
 export default App;
